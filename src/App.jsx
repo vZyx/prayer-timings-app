ebf00c424904835f5b5ce8e41aa15c2e16d53946
Ziyad Saeed Alghamdi <ziyadsaeedgh@gmail.com>
2024-09-27 16:31:31 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import MainContent from "./Components/MainContent";
+import Container from '@mui/material/Container';
+
+function App() {
+    return (
+        <>
+            <div
+                style={{
+                    
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100vw",
+                }}
+            >
+                <Container maxWidth="xl">
+                    <MainContent />
+                </Container>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/MainContent.jsx
@@ -0,0 +1,261 @@
+	//API
+    // const data = await axios.get(
+	// 	"https://api.aladhan.com/v1/timingsByCity?country=SA&city=Riyadh"
+import "../App.css";
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import Prayer from "./Prayer";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import axios from "axios";
+
+import moment from "moment";
+import { useForkRef } from "@mui/material";
+import "moment/dist/locale/ar-dz";
+moment.locale("ar");
+moment.locale("ar");
+//images
+import fajrImage from "../assets/fajr-prayer.png";
+import dhuhrImage from "../assets/dhuhr-prayer.png";
+import asrImage from "../assets/asr-prayer.png";
+import maghribImage from "../assets/maghrib-prayer.jpg"
+import ishaImage from "../assets/isha-prayer.png";
+import { avatarClasses } from "@mui/material";
+
+export default function MainContent() {
+	// STATES
+	const [nextPrayerIndex, setNextPrayerIndex] = useState(2);
+	const [timings, setTimings] = useState({
+		Fajr: "04:20",
+		Dhuhr: "11:50",
+		Asr: "15:18",
+		Sunset: "18:03",
+		Isha: "19:33",
+	});
+
+	const [remainingTime, setRemainingTime] = useState("");
+
+	const [selectedCity, setSelectedCity] = useState({
+		displayName: "مكة المكرمة",
+		apiName: "Makkah al Mukarramah",
+	});
+
+	const [today, setToday] = useState("");
+
+	const avilableCities = [
+		{
+			displayName: "مكة المكرمة",
+			apiName: "Makkah al Mukarramah",
+		},
+		{
+			displayName: "الرياض",
+			apiName: "Riyadh",
+		},
+		{
+			displayName: "الدمام",
+			apiName: "Dammam",
+		},
+	];
+
+	const prayersArray = [
+		{ key: "Fajr", displayName: "الفجر" },
+		{ key: "Dhuhr", displayName: "الظهر" },
+		{ key: "Asr", displayName: "العصر" },
+		{ key: "Sunset", displayName: "المغرب" },
+		{ key: "Isha", displayName: "العشاء" },
+	];
+
+	const getTimings = async () => {
+		console.log("calling the api");
+		const response = await axios.get(
+			`https://api.aladhan.com/v1/timingsByCity?country=SA&city=${selectedCity.apiName}`
+		);
+		setTimings(response.data.data.timings);
+	};
+	useEffect(() => {
+		getTimings();
+	}, [selectedCity]);
+
+	useEffect(() => {
+		let interval = setInterval(() => {
+			console.log("calling timer");
+			setupCountdownTimer();
+		}, 1000);
+
+		const t = moment();
+		setToday(t.format("MMM Do YYYY | h:mm"));
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [timings]);
+
+
+	const setupCountdownTimer = () => {
+		const momentNow = moment();
+
+		let prayerIndex = 2;
+
+		if (
+			momentNow.isAfter(moment(timings["Fajr"], "hh:mm")) &&
+			momentNow.isBefore(moment(timings["Dhuhr"], "hh:mm"))
+		) {
+			prayerIndex = 1;
+		} else if (
+			momentNow.isAfter(moment(timings["Dhuhr"], "hh:mm")) &&
+			momentNow.isBefore(moment(timings["Asr"], "hh:mm"))
+		) {
+			prayerIndex = 2;
+		} else if (
+			momentNow.isAfter(moment(timings["Asr"], "hh:mm")) &&
+			momentNow.isBefore(moment(timings["Sunset"], "hh:mm"))
+		) {
+			prayerIndex = 3;
+		} else if (
+			momentNow.isAfter(moment(timings["Sunset"], "hh:mm")) &&
+			momentNow.isBefore(moment(timings["Isha"], "hh:mm"))
+		) {
+			prayerIndex = 4;
+		} else {
+			prayerIndex = 0;
+		}
+
+		setNextPrayerIndex(prayerIndex);
+
+		// now after knowing what the next prayer is, we can setup the countdown timer by getting the prayer's time
+		const nextPrayerObject = prayersArray[prayerIndex];
+		const nextPrayerTime = timings[nextPrayerObject.key];
+		const nextPrayerTimeMoment = moment(nextPrayerTime, "hh:mm");
+
+		let remainingTime = moment(nextPrayerTime, "hh:mm").diff(momentNow);
+
+		if (remainingTime < 0) {
+			const midnightDiff = moment("23:59:59", "hh:mm:ss").diff(momentNow);
+			const fajrToMidnightDiff = nextPrayerTimeMoment.diff(
+				moment("00:00:00", "hh:mm:ss")
+			);
+
+			const totalDiffernce = midnightDiff + fajrToMidnightDiff;
+
+			remainingTime = totalDiffernce;
+		}
+		console.log(remainingTime);
+
+		const durationRemainingTime = moment.duration(remainingTime);
+
+		setRemainingTime(
+			`${durationRemainingTime.seconds()} : ${durationRemainingTime.minutes()} : ${durationRemainingTime.hours()}`
+		);
+		console.log(
+			"duration issss ",
+			durationRemainingTime.hours(),
+			durationRemainingTime.minutes(),
+			durationRemainingTime.seconds()
+		);
+	};
+	const handleCityChange = (event) => {
+		const cityObject = avilableCities.find((city) => {
+			return city.apiName == event.target.value;
+		});
+		console.log("the new value is ", event.target.value);
+		setSelectedCity(cityObject);
+	};
+
+	return (
+		<>
+			{/* TOP ROW */}
+			<Grid container>
+				<Grid xs={6}>
+					<div>
+						<h2>{today}</h2>
+						<h1>{selectedCity.displayName}</h1>
+					</div>
+				</Grid>
+
+				<Grid xs={6}>
+					<div>
+						<h2>
+							متبقي حتى صلاة{" "}
+							{prayersArray[nextPrayerIndex].displayName}
+						</h2>
+						<h1>{remainingTime}</h1>
+					</div>
+				</Grid>
+			</Grid>
+			{/*== TOP ROW ==*/}
+
+			<Divider style={{ borderColor: "white", opacity: "0.1" }} />
+
+			{/* PRAYERS CARDS */}
+			<Stack
+				direction="row"
+				justifyContent={"space-around"}
+				style={{ marginTop: "50px" }}
+			>
+				<Prayer
+					name="الفجر"
+					time={timings.Fajr}
+					image={fajrImage}
+				/>
+				<Prayer
+					name="الظهر"
+					time={timings.Dhuhr}
+					image={dhuhrImage}
+				/>
+				<Prayer
+					name="العصر"
+					time={timings.Asr}
+					image={asrImage}
+				/>
+				<Prayer
+					name="المغرب"
+					time={timings.Sunset}
+					image={maghribImage}
+				/>
+				<Prayer
+					name="العشاء"
+					time={timings.Isha}
+					image={ishaImage}
+				/>
+			</Stack>
+			{/*== PRAYERS CARDS ==*/}
+
+			{/* SELECT CITY */}
+			<Stack
+				direction="row"
+				justifyContent={"center"}
+				style={{ marginTop: "40px" }}
+			>
+				<FormControl style={{ width: "20%" }}>
+					<InputLabel id="demo-simple-select-label">
+						<span style={{ color: "white" }}>المدينة</span>
+					</InputLabel>
+					<Select
+						style={{ color: "white" }}
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						// value={age}
+						label="Age"
+						onChange={handleCityChange}
+					>
+						{avilableCities.map((city) => {
+							return (
+								<MenuItem
+									value={city.apiName}
+									key={city.apiName}
+								>
+									{city.displayName}
+								</MenuItem>
+							);
+						})}
+					</Select>
+				</FormControl>
+			</Stack>
+		</>
+	);
+}
